6440289fa7abc5343fe135d081283f2bae2c4175
nehabht <ne.bhatti2@icloud.com>
2022-04-05 13:08:53 +0200

    add footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@ Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 let limit = 0
 
 
-// cambia il valore di limit in base all'option del livello selezionato
+// cambia il valore di limit e cols in base all'option del livello selezionato
 // seleziona form dom
 
 
@@ -33,25 +33,27 @@ formElement.addEventListener("submit", function (event) {
     
         if (levelElement === "1"){
             limit = 100
+            cols = 10
             console.log('easy')
-            generateGrid('.cells', 'div', 'cell', limit)
+            generateGrid('.cells', 'div', 'cell', limit, cols)
             generateNumbers(limit)
             
             
         }else if (levelElement === "2"){
             limit = 81
+            cols = 9
             console.log('e')
-            generateGrid('.cells', 'div', 'cell', limit)
+            generateGrid('.cells', 'div', 'cell', limit, cols)
             generateNumbers(limit)
             
             
         }else if (levelElement === "3"){
             limit = 49
+            cols = 7
             console.log('d')
-            generateGrid('.cells', 'div', 'cell', limit)
+            generateGrid('.cells', 'div', 'cell', limit, cols)
             generateNumbers(limit)
-            
-            
+ 
 
         }
     
@@ -62,33 +64,25 @@ formElement.addEventListener("submit", function (event) {
     selectElements('.cell')
 
 
-
-
 })
-
-
-
-
-
-
 
 
 // genero una griglia
 
-function generateGrid(selector, tag_name, class_name, limit) {
+function generateGrid(selector, tag_name, class_name, limit, cols) {
     const cellsElement = document.querySelector(selector)
-
     cellsElement.innerHTML = "";
   
     for (let i = 1; i <= limit; i++) {
       const cellItem = document.createElement(tag_name);
       cellItem.classList.add(class_name)
       cellsElement.append(cellItem)
+    //   divido lo spazio che occupa la singola cella in base al numero di colonne
+      cellItem.style.width = `calc( 100% / ${cols})`
+
     }
   
 }
-
-
 
 
 // generateGrid('.cells', 'div', 'cell', limit)
@@ -117,9 +111,7 @@ function generateNumbers(limit){
 function selectElements(selector){
     const cells = document.querySelectorAll(selector);
     const numbers = generateNumbers(limit)
-
-    
-    
+    let cell;
 
     for (let i = 0; i < cells.length; i++) {
         const cell = cells[i];
